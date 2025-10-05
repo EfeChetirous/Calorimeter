f5fe48a2a28c2e6cff21eeff64282fb78aa37071
efecan çetir <efecan.cetir@outlook.com>
2025-10-05 22:03:21 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Calorimeter',
+  webDir: 'www'
+};
+
+export default config;
