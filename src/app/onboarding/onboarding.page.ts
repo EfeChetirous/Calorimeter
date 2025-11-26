0eee508737bef164788b29a20ba0b244b9a58417
efecan çetir <efecan.cetir@outlook.com>
2025-11-26 11:29:49 +0300

    username eklendi

--- src/app/onboarding/onboarding.page.ts
+++ src/app/onboarding/onboarding.page.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { 
-  IonContent, 
-  IonButton, 
-  IonItem, 
-  IonInput, 
-  IonSelect, 
-  IonSelectOption, 
+import {
+  IonContent,
+  IonButton,
+  IonItem,
+  IonInput,
+  IonSelect,
+  IonSelectOption,
   IonLabel,
-  IonIcon
+  IonIcon,
+  ToastController
 } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 
 interface UserData {
+  username: string;
   email: string;
   gender: string;
   height: number | null;
@@ -27,20 +29,21 @@ interface UserData {
   styleUrls: ['./onboarding.page.scss'],
   standalone: true,
   imports: [
-    IonContent, 
-    IonButton, 
-    IonItem, 
-    IonInput, 
-    IonSelect, 
-    IonSelectOption, 
+    IonContent,
+    IonButton,
+    IonItem,
+    IonInput,
+    IonSelect,
+    IonSelectOption,
     IonLabel,
     IonIcon,
-    CommonModule, 
+    CommonModule,
     FormsModule
   ]
 })
 export class OnboardingPage implements OnInit {
   user: UserData = {
+    username: '',
     email: '',
     gender: '',
     height: null,
@@ -53,36 +56,61 @@ export class OnboardingPage implements OnInit {
     { value: 'female', label: 'Female' }
   ];
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private toastController: ToastController
+  ) { }
 
   ngOnInit() {
     // Component initialization
   }
 
-  onContinue() {
+  async onContinue() {
     // Validate form data
     if (this.isFormValid()) {
       console.log('User data:', this.user);
-      
+
       // TODO: Send data to backend API
       // this.sendDataToBackend();
-      
+
       // For now, navigate to home page
       this.router.navigate(['/home']);
     } else {
       console.log('Form validation failed');
-      // TODO: Show validation error message
+      await this.showValidationError();
     }
   }
 
   private isFormValid(): boolean {
     return !!(
+      this.user.username &&
       this.user.email &&
+      this.isValidEmail(this.user.email) &&
       this.user.gender &&
       this.user.height &&
       this.user.weight &&
       this.user.targetCalorie
     );
+  }
+
+  private isValidEmail(email: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  private async showValidationError() {
+    let message = 'Please fill in all fields.';
+    if (this.user.email && !this.isValidEmail(this.user.email)) {
+      message = 'Please enter a valid email address.';
+    }
+
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'bottom',
+      color: 'danger'
+    });
+    toast.present();
   }
 
   // TODO: Implement API call to send user data to backend
